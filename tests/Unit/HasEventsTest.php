6727d787fb604bb18ad58d0542e617a8628822f4
Quetzy Garcia <quetzyg@altek.org>
2018-12-26 01:36:29 +0000

    tests(HasEvents): added unit tests

---
+++ tests/Unit/HasEventsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Altek\Eventually\Tests\Integration\BelongsToMany;
+
+use Altek\Eventually\Tests\EventuallyTestCase;
+use Altek\Eventually\Tests\Models\User;
+
+class HasEventsTest extends EventuallyTestCase
+{
+    /**
+     * @test
+     */
+    public function itAssuresThePivotEventsAreObservable(): void
+    {
+        $user = new User();
+
+        $this->assertContains('toggling', $user->getObservableEvents());
+        $this->assertContains('toggled', $user->getObservableEvents());
+        $this->assertContains('syncing', $user->getObservableEvents());
+        $this->assertContains('synced', $user->getObservableEvents());
+        $this->assertContains('updatingExistingPivot', $user->getObservableEvents());
+        $this->assertContains('updatedExistingPivot', $user->getObservableEvents());
+        $this->assertContains('attaching', $user->getObservableEvents());
+        $this->assertContains('attached', $user->getObservableEvents());
+        $this->assertContains('detaching', $user->getObservableEvents());
+        $this->assertContains('detached', $user->getObservableEvents());
+    }
+}
