d3f7b7e3b47aa93a14d02d11d9389a45a0bad0ff
Quetzy Garcia <quetzyg@altek.org>
2018-12-26 01:37:59 +0000

    tests(Integration): added FirePivotEvent tests

---
+++ tests/Events/CustomEvent.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Altek\Eventually\Tests\Events;
+
+class CustomEvent
+{
+    //
+}
---
+++ tests/Integration/FirePivotEventTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Altek\Eventually\Tests\Integration;
+
+use Altek\Eventually\Tests\Events\CustomEvent;
+use Altek\Eventually\Tests\EventuallyTestCase;
+use Altek\Eventually\Tests\Models\Article;
+use Altek\Eventually\Tests\Models\User;
+use Illuminate\Support\Facades\Event;
+
+class FirePivotEventTest extends EventuallyTestCase
+{
+    /**
+     * @test
+     */
+    public function itDoesNotFireEventsDueToDispatcherBeingUnset(): void
+    {
+        User::unsetEventDispatcher();
+
+        $user = factory(User::class)->create();
+        $articles = factory(Article::class, 2)->create();
+
+        $this->assertCount(0, $user->articles()->get());
+        $this->assertTrue($user->articles()->attach($articles));
+        $this->assertCount(2, $user->articles()->get());
+    }
+
+    /**
+     * @test
+     */
+    public function itPreventsModelsFromBeingAttachedViaCustomEventListener(): void
+    {
+        $articles = factory(Article::class, 2)->create();
+
+        $user = new class() extends User {
+            protected $dispatchesEvents = [
+                'attaching' => CustomEvent::class,
+            ];
+        };
+
+        Event::listen(CustomEvent::class, function () {
+            return false;
+        });
+
+        $this->assertFalse($user->articles()->attach($articles));
+    }
+}
